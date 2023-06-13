a2b0747a309ae568a1b13509de756713c58a4e03
YagoJanos <yjquaranta@gmail.com>
2023-06-13 11:29:56 -0300

    Oauth2 authentication with google and github

---
+++ src/main/java/com/yago/oauth2/social/SocialApplication.java
@@ -0,0 +1,13 @@
+package com.yago.oauth2.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+}
